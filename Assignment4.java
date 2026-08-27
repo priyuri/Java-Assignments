545ea2ec1be2186e8ae8edbebf22c6fbf1484b60
priyuri <mayuridurge04@gmail.com>
2026-08-27 09:02:46 +0530

    added assignment 4 of my lecture

--- Assignment4.java
+++ Assignment4.java
@@ -1,36 +1,166 @@
 public class Assignment4 {
     public static void main(String[] args) {
-        //inverted pattern
-        int n=4;
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=i; j++){
+        // inverted pattern
+        int n = 4;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        //example 2 hollow rectangle pattern
-        int m=5;
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=m; j++){
-                if(i==1 || j==1 || i==m || j==m){
+        // example 2 hollow rectangle pattern
+        int m = 5;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == m || j == m) {
                     System.out.print("*");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
 
-        //example 3 inverted and rotated half pyramid pattern
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
+        // example 3 inverted and rotated half pyramid pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int k=1; k<=i; k++){
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        // example 4 Inverted half pyramid with number system
+        for (int i = m; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // example 5 floyd's triangle pattern
+        int num = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+
+        // example 6 - 0-1 traingle pattern
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+
+        // example 7 Butterfly pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= (n - i) * 2; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= (n - i) * 2; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // example 8 solid rhombus pattern
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= (m - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= m; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // problem 9 number pyramid pattern
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= (m - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        // problem 10 diamond pattern
+        // upper part
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower part
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // problem 11 palindromic pattern with number pattern
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j < m - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // problem 12 hollow rhombus
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= (m - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= m; k++) {
+                if (i == 1 || k == 1 || i == m || k == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
